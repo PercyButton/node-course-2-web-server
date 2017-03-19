5fd436f07b073dcdf7359ef9dca4d21b12a26e87
PercyButton <v1beta@hotmail.com>
2017-03-19 13:34:20 +0800

    Add projects file

--- server.js
+++ server.js
@@ -43,9 +43,14 @@ app.get('/', (req, res) => {
 });
 
 app.get('/about', (req, res) => {
-  // res.send('About Page');
   res.render('about.hbs', {
     pageTitle: 'About Page'
+  });
+});
+
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'My Projects'
   });
 });
 
